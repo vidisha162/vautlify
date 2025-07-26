baa2a065d6eb47ef2e2ec8f5b5a4d842fc3d6565
vidisha <vidishafirani66@gmail.com>
2025-07-26 21:36:57 +0530

    commit this changes

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,7 @@ import secretRoutes from "./routes/SecretRoutes.js";
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000; 
 app.use(cors());
 app.use(express.json());
 
